c08bcd9f2432a2893bba5f7e557d3cfd06b40e86
lhn-debug <luohaonan_666@163.com>
2020-02-07 20:31:26 +0800

    code

---
+++ C++/200207_pair/pair.h
@@ -0,0 +1,16 @@
+template<class T1, class T2>
+struct pair
+{
+	typedef T1 first_type;
+	typedef T2 second_type;
+	T1 first;
+	T2 second;
+	pair()
+		:first(T1()),
+		second(T2())
+	{}
+	pair(const T1& a, const T2& b)
+		:first(a),
+		second(b)
+	{}
+};
